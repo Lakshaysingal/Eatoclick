991ad86b8cfe27de8446460f53eeb9c50be31193
Lakshay Singal <singallakshay84@gmail.com>
2025-08-06 19:00:15 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const [cartItem, setCartItems] = useState({});
-  const URl = "http://localhost:4000"
+  const URl = "https://eatoclick-1.onrender.com"
   const [token , setToken] = useState("")
   const [food_list,setFoodList] = useState([])
 
@@ -83,3 +83,4 @@ const StoreContextProvider = (props) => {
 };
 
 export default StoreContextProvider;
+
